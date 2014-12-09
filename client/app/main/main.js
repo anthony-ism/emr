91cdfb124cc656162b50bd692740e46dc95000a3
anthony-ism <anthony@isminc.com>
2014-12-08 20:46:53 -0500

    inital commit

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('meApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
